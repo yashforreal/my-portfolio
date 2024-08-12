b4b693fcfa6926dfe5ac06c817479cadd109c545
Yash <yka.yash@gmail.com>
2024-08-12 23:18:09 +0530

    added data file

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const data = {
+    name: "Yash Aswani",
+    about_me: "I'm a software engineer who loves creating software that makes a difference. I have a solid background in computer science and experience in building various types of applications. I enjoy working with different programming languages like Python, Java, and JavaScript. I’m a problem-solver at heart and enjoy learning new things, especially when it comes to the latest tech trends like AI and blockchain. I believe in working together as a team to tackle challenges, and I’m always eager to share what I know with others, whether through mentoring or writing. When I’m not coding, I like to stay updated on new technologies, read about software design, or contribute to open-source projects. I’m passionate about continuous learning and improving my skills.",
+    skills: [
+        { name: 'Java', stars: 7 },
+        { name: 'JavaScript', stars: 7 },
+        { name: 'TypeScript', stars: 7 },
+        { name: 'React', stars: 6 },
+        { name: 'C++', stars: 6 },
+        { name: 'C#', stars: 6 },
+        { name: 'Python', stars: 6 },
+        { name: 'SQL', stars: 5 },
+    ]
+}
